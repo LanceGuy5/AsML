66c341a3a37202ce0ac284b7e5607cde4903d8aa
LanceGuy5 <lanceforresth@gmail.com>
2025-03-24 19:50:36 -0400

    work

--- hello.s
+++ hello.s
@@ -1,19 +1,19 @@
 .global _main
 .extern _printf
 
-.section __DATA,__data
+.section __DATA,__data          // switch to MacOS data section
 message:
     .asciz "Hello, ARM64 macOS!\n"
 
-.section __TEXT,__text
+.section __TEXT,__text          // switch to MacOS executable section
 _main:
-    stp x29, x30, [sp, -16]!    // Save frame pointer and link register
+    STP x29, x30, [sp, -16]!    // Save frame pointer and link register
 
-    adrp x0, message@PAGE       // Load the page of the message address
-    add x0, x0, message@PAGEOFF // Add the offset within the page
+    ADRP x0, message@PAGE       // Load the page of the message address
+    ADD x0, x0, message@PAGEOFF // Add the offset within the page
 
-    bl _printf                  // Call printf
+    BL _printf                  // Call printf
 
-    ldp x29, x30, [sp], 16      // Restore frame pointer and link register
-    mov x0, 0                   // Return 0
-    ret
+    LDP x29, x30, [sp], 16      // Restore frame pointer and link register
+    MOV x0, 0                   // Return 0
+    RET
